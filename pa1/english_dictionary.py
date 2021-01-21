620badadcb02cda782e62a3676f7b0e9ba4fe1c8
Jake Underland <jaked0626@cs.uchicago.edu>
2021-01-21 07:52:52 -0600

    finished pa1

--- pa1/english_dictionary.py
+++ pa1/english_dictionary.py
@@ -7,7 +7,7 @@
 # Revised: August 2015, AMR
 #   December 2017, AMR
 #
-# YOUR NAME HERE
+# Jake Underland
 
 import os
 import sys
@@ -41,9 +41,10 @@ class EnglishDictionary(object):
 
         Returns: boolean
         '''
-        # ADD YOUR CODE HERE AND REPLACE THE False
-        # IN THE RETURN WITH A SUITABLE RETURN VALUE.
-        return False
+        try:
+            return self.words.traverse_nodes(w).final
+        except:  # word does not exist within self.words
+            return False
 
     def num_completions(self, prefix):
         '''
@@ -55,17 +56,11 @@ class EnglishDictionary(object):
 
         Returns: int
         '''
-        # IMPORTANT: When you replace this version with the trie-based
-        # version, do NOT compute the number of completions simply as
-        #
-        #    len(self.get_completions(prefix))
-        #
-        # See PA writeup for more details.
 
-        # ADD YOUR CODE HERE AND REPLACE THE ZERO IN THE RETURN WITH A
-        # SUITABLE RETURN VALUE.
-
-        return 0
+        try:
+            return self.words.traverse_nodes(prefix).count
+        except:  # prefix does not exist within self.words
+            return 0
 
     def get_completions(self, prefix):
         '''
@@ -77,23 +72,70 @@ class EnglishDictionary(object):
 
         Returns: list of strings.
         '''
+        if self.is_word(prefix):
+            return [""] + self.words.traverse_nodes(prefix).find_words()
+        else: 
+            return self.words.traverse_nodes(prefix).find_words()
 
-        # ADD YOUR CODE HERE AND REPLACE THE EMPTY LIST
-        # IN THE RETURN WITH A SUITABLE RETURN VALUE.
-
-        return []
 
 
 class TrieNode(object):
     def __init__(self):
-        ### REPLACE pass with appropriate documentation and code
-        pass
+        '''
+        Constructor
+        '''
+        self.children = {} # keys will be letters, values the node class
+        self.count = 0
+        self.final = False
 
     def add_word(self, word):
-        ### REPLACE pass with appropriate documentation and code
-        pass
+        '''
+        Adds word to a node, creating a node for each letter in the word.
 
-    ### ADD ANY EXTRA METHODS HERE.
+        Inputs:
+          word (string): The word
+        '''
+        self.count += 1
+        if not word:  # base case. No more letters indicates end of word
+            self.final = True
+
+        else:  # recursive case. If new letter, create new node.
+            if not self.children.get(word[0]):
+                self.children[word[0]] = TrieNode()
+            self.children[word[0]].add_word(word[1:])
+
+    def traverse_nodes(self, word):
+        '''
+        Of a word already in self, navigates to the node of the last letter in 
+        the word. 
+        
+        Inputs:
+          word (string): the word. Must already be within self.
+
+        Returns the TrieNode object for the last letter in word
+        '''
+        if not word:
+            return self
+        else:
+            return self.children[word[0]].traverse_nodes(word[1:])
+
+    def find_words(self, prefix=""):
+        '''
+        Lists all complete words that are found under a given node. 
+
+        Inputs:
+          prefix (string): stores the route from the initial node to the
+          current node. Should not be manually entered. 
+        
+        Returns a list of words (strings) under the node. 
+        '''
+        words = []
+        for child_char, child in self.children.items():
+            if child.final:
+                words += [prefix + child_char]
+            words += child.find_words(prefix + child_char)
+        
+        return words
 
 if __name__ == "__main__":
     autocorrect_shell.go("english_dictionary")
